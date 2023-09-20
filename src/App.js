b22f179ebfb48c9c1c1d7580fb4a7cdf2b2af7a9
sara segura <sarasegura224@gmail.com>
2023-09-20 11:12:33 -0500

    Se crean los archivos en la carpeta csr, TodoList.js, TodoSearch.js y TodoCounter.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './platzi.webp';
-import './App.css';
-
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import "./App.css";
+// Componente de react JSX
+// elementos de react va en minusculas
+// Si comensaran en minusculas serian componentes de react
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      {/* <CreateTodoButton /> */}
     </div>
   );
 }
 
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
 export default App;
