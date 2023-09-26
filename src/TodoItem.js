adb8b79018e7fb6c5e58762ac2e265a32c70dff7
sara segura <sarasegura224@gmail.com>
2023-09-26 16:25:03 -0500

    Se agregaron estilos.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,43 @@
+import React, { useState } from "react";
+
 import "./TodoItem.css";
+import "./assets/ckeck.svg";
 
 function TodoItem(props) {
+  const [iconColor, setIconColor] = useState("#ffff"); // Estado para controlar el color del icono
+
+  const handleIconClick = () => {
+    setIconColor("#ff26ba"); // Cambiar el color del icono cuando se hace clic
+  };
+
   return (
     <li className="containerItem">
-      <span className="filled">✅ </span>
+      <span className="ckeck" onClick={handleIconClick}>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 512 512"
+            fill={iconColor} // Usa el color del estado
+          >
+            <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
+          </svg>
+        </div>
+      </span>
       <p className="textList">{props.text}</p>
-      <span className="remove">X</span>
+      <span className="delete">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 576 512"
+            fill="#ffff"
+            id="deleteIcon"
+          >
+            <path d="M576 128c0-35.3-28.7-64-64-64H205.3c-17 0-33.3 6.7-45.3 18.7L9.4 233.4c-6 6-9.4 14.1-9.4 22.6s3.4 16.6 9.4 22.6L160 429.3c12 12 28.3 18.7 45.3 18.7H512c35.3 0 64-28.7 64-64V128zM271 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
+          </svg>
+        </div>
+      </span>
     </li>
   );
 }
