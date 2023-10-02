eaee1fab526be2f187f31a90ee6f236f3b8af190
sara segura <sarasegura224@gmail.com>
2023-10-02 11:18:50 -0500

    Se crea logica para eliminar los dotos.

--- src/App.js
+++ src/App.js
@@ -37,7 +37,21 @@ function App() {
     return todoText.includes(searchText);
   });
 
-  console.log("Los usuarios buscan todos de " + searchValue);
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex((todo) => todo.text == text);
+
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
 
   return (
     <>
@@ -52,6 +66,8 @@ function App() {
               key={todo.text}
               text={todo.text}
               completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
             />
           ))}
         </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./TodoItem.css";
 import "./assets/ckeck.svg";
@@ -6,15 +6,18 @@ import "./assets/ckeck.svg";
 function TodoItem(props) {
   const completed = props?.completed;
 
+  //className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}
   return (
-    <li className="containerItem">
+    <li className={completed ? "containerItem" : "containerItem--complete"}>
       <span className="ckeck">
         <div>
           <svg
+            onClick={props.onComplete}
             xmlns="http://www.w3.org/2000/svg"
             height="1em"
             viewBox="0 0 512 512"
-            fill={completed ? "#ff26ba" : "#fff"} // Usa el color del estado
+            fill={completed ? "#ff26ba" : "#fff"}
+            className="iconckeck"
           >
             <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z" />
           </svg>
@@ -24,6 +27,7 @@ function TodoItem(props) {
       <span className="delete">
         <div>
           <svg
+            onClick={props.onDelete}
             xmlns="http://www.w3.org/2000/svg"
             height="1em"
             viewBox="0 0 576 512"
