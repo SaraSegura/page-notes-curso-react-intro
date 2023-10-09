d39be4e3922db5a7cdc3367cbd99003ecece36d5
sara segura <sarasegura224@gmail.com>
2023-10-09 10:19:07 -0500

    Se creo la logica para crear un nuevo todo.

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,16 +1,45 @@
 import React from "react";
 import "./TodoForm.css";
+import { TodoContext } from "../TodoContext";
 
 function TodoForm() {
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
+
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  const OnCancel = () => {
+    setOpenModal(false);
+  };
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label>Escribe tu nueva tarea</label>
-      <textarea placeholder="Cortar cebolla" />
+      <textarea
+        placeholder="Cortar cebolla"
+        value={newTodoValue}
+        onChange={onChange}
+      />
       <div className="TodoForm-buttonContainer">
-        <button className="TodoForm-button TodoForm-button--cancel">
+        <button
+          type="button"
+          className="TodoForm-button TodoForm-button--cancel"
+          onClick={OnCancel}
+        >
           Cancelar
         </button>
-        <button className="TodoForm-button TodoForm-button--add">Añadir</button>
+        <button type="submit" className="TodoForm-button TodoForm-button--add">
+          Añadir
+        </button>
       </div>
     </form>
   );
