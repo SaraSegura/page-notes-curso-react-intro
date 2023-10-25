450ab02ad69dab668ad96769f8e4e162ea0c406c
sara segura <sarasegura224@gmail.com>
2023-10-25 10:04:39 -0500

    Se elimino react context

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,16 @@
-import React from "react";
-import { AppUI } from "./AppUI";
-import { TodoProvider } from "../TodoContext";
-
+import React, { useState, useEffect } from "react";
+import { useTodos } from "./useTodos";
+import { TodoList } from "../TodoList";
+import { TodoItem } from "../TodoItem";
+import { TodosLoading } from "../TodosLoading";
+import { TodosError } from "../TodosError";
+import { EmptyTodos } from "../EmptyTodos";
+import { TodoForm } from "../TodoForm";
+import { CreateTodoButton } from "../CreateTodoButton";
+import { Modal } from "../Modal";
+import TodoHeader from "../TodoHeader";
+import { TodoCounter } from "../TodoCounter";
+import { TodoSearch } from "../TodoSearch";
 import "./App.css";
 
 // Componente de react JSX
@@ -9,10 +18,85 @@ import "./App.css";
 // Si comensaran en minusculas serian componentes de react
 
 function App() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    completedTodos,
+    totalTodos,
+    searchValue,
+    setSearchValue,
+    addTodo,
+  } = useTodos();
+
+  // Estado para controlar el tamaño de la pantalla
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isMediumScreen, setIsMediumScreen] = useState(false); // Add this line
+
+  useEffect(() => {
+    // Verificar el tamaño de la ventana al cargar la página
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      setIsSmallScreen(windowWidth < 769);
+      setIsMediumScreen(windowWidth >= 769 && windowWidth < 1024); // Update this line
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+        <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      </TodoHeader>
+      <div className="container">
+        <TodoList>
+          {loading && (
+            <>
+              <TodosLoading />
+              <TodosLoading />
+              <TodosLoading />
+            </>
+          )}
+          {error && <TodosError />}
+          {!loading && searchedTodos.length === 0 && <EmptyTodos />}
+
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+
+        <CreateTodoButton
+          setOpenModal={setOpenModal}
+          isVisible={!isSmallScreen}
+          isVisibleMediumScreen={!isMediumScreen}
+        />
+
+        {!isSmallScreen && !isMediumScreen && !openModal && <TodoForm />}
+
+        {(openModal || isMediumScreen) && (
+          <Modal>
+            <TodoForm addTodo={addTodo} setOpenModal={setOpenModal} />
+          </Modal>
+        )}
+      </div>
+    </>
   );
 }
 
